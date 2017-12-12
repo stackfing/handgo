28a40364f705f32498ea8e975c8f13dbf28d421d
stackfing <stackfing@gmail.com>
2017-12-12 13:36:47 +0800

    init

---
+++ src/main/java/com/stackfing/handgo/HandgoApplication.java
@@ -0,0 +1,12 @@
+package com.stackfing.handgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandgoApplication.class, args);
+	}
+}
