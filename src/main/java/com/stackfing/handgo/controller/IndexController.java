97a9ee50472a19575931eb747357e6713711c145
stackfing <stackfing@gmail.com>
2017-12-19 22:18:24 +0800

    这些是什么..

--- src/main/java/com/stackfing/handgo/controller/IndexController.java
+++ src/main/java/com/stackfing/handgo/controller/IndexController.java
@@ -6,8 +6,12 @@ import com.stackfing.handgo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 @Controller
@@ -26,4 +30,28 @@ public class IndexController {
 		return "index";
 	}
 
+	@RequestMapping(value="/gouploadimg", method = RequestMethod.GET)
+	public String goUploadImg() {
+		//跳转到 templates 目录下的 uploadimg.html
+		return "uploadimg";
+	}
+
+	@GetMapping("/upload")
+	public String upload() {
+		return "admin/upload";
+	}
+
+	//处理文件上传
+	@RequestMapping(value="/testuploadimg", method = RequestMethod.POST)
+	public @ResponseBody
+	String uploadImg(@RequestParam("file") MultipartFile file,
+					 HttpServletRequest request) throws Exception {
+		String fileName = file.getOriginalFilename();
+		FileOutputStream fos = new FileOutputStream(new File("/home/fing/" + fileName));
+		fos.write(file.getBytes());
+		fos.flush();
+		fos.close();
+		//返回json
+		return "uploadimg success";
+	}
 }
