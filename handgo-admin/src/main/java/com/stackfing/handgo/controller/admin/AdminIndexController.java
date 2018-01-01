0f29ede39c41ce1671a75c556aa74dc39a4c6aee
stackfing <stackfing@gmail.com>
2018-01-01 22:53:04 +0800

    类型树

--- handgo-admin/src/main/java/com/stackfing/handgo/controller/admin/AdminIndexController.java
+++ handgo-admin/src/main/java/com/stackfing/handgo/controller/admin/AdminIndexController.java
@@ -63,4 +63,5 @@ public class AdminIndexController {
 		}
 		return "";
 	}
+
 }
--- handgo-admin/src/main/java/com/stackfing/handgo/controller/admin/ProductTypesController.java
+++ handgo-admin/src/main/java/com/stackfing/handgo/controller/admin/ProductTypesController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -16,11 +17,36 @@ public class ProductTypesController {
 
 	@Autowired
 	private ProductTypeService productTypeService;
+//
+//	@GetMapping("")
+//	public String types(ModelMap modelMap) {
+//		List<ProductType> allType = productTypeService.findAllType();
+//		modelMap.put("allType", allType);
+//		return "admin/types";
+//	}
 
-	@GetMapping("")
-	public String types(ModelMap modelMap) {
-		List<ProductType> allType = productTypeService.findAllType();
-		modelMap.put("allType", allType);
+	@GetMapping("/types")
+	public String toType() {
 		return "admin/types";
+	}
+
+	@GetMapping("/getTypes")
+	@ResponseBody
+	public String getType() {
+		return "[{ \n" +
+				"    name: 'asd'\n" +
+				"    ,children: [{\n" +
+				"      name: 'asd'\n" +
+				"    }]\n" +
+				"  }, {\n" +
+				"    name: 'asd'\n" +
+				"    ,children: [{\n" +
+				"      name: 'asd'\n" +
+				"      ,alias: 'bb' \n" +
+				"      ,id: '123' \n" +
+				"    }, {\n" +
+				"      name: 'sdf'\n" +
+				"    }]\n" +
+				"  }] ";
 	}
 }
