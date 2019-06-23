acc17901005557eed52f41ff3101186f74327610
stackfing <stackfing@gmail.com>
2019-06-23 13:41:40 +0800

    用户登录返回信息

--- handgo-admin/src/main/java/com/stackfing/admin/enums/ResultCode.java
+++ handgo-admin/src/main/java/com/stackfing/admin/enums/ResultCode.java
@@ -13,7 +13,15 @@ public enum ResultCode {
 
 	UNKNOW_ERROR(-1, "未知错误"),
 	SUCCESS(200, "响应成功"),
-	ID_NOT_EQUALS(10003, "ID 不一致")
+	ID_NOT_EQUALS(10003, "ID 不一致"),
+	SERVER_INTERNAL_ERROR(500, "服务器内部错误"),
+
+
+
+
+	LOGIN_SUCCESS(20001, "用户登录成功"),
+	LOGIN_FAILD(20002, "登录失败，请检查用户名或密码"),
+	PASSWORD_NOT_EQUALS(20003, "密码错误"),
 	;
 
 
