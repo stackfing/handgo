64b68313b3c97541a735431c24f0ea5eaa032119
stackfing <stackfing@gmail.com>
2019-06-23 13:42:30 +0800

    用户信息异常处理

---
+++ handgo-admin/src/main/java/com/stackfing/admin/Exception/UserNotFoundException.java
@@ -0,0 +1,12 @@
+package com.stackfing.admin.Exception;
+
+/**
+ * @Author: fing
+ * @Description:
+ * @Date: 下午1:22 19-6-23
+ */
+public class UserNotFoundException extends HandgoException {
+	public UserNotFoundException(String msg) {
+		super(msg);
+	}
+}
