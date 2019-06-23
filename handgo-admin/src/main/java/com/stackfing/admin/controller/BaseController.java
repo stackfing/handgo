76d2acdca57f0ddb5cd1b3887dc90ab8bca4a4ed
stackfing <stackfing@gmail.com>
2019-06-23 22:50:30 +0800

    通用删除方法

--- handgo-admin/src/main/java/com/stackfing/admin/controller/BaseController.java
+++ handgo-admin/src/main/java/com/stackfing/admin/controller/BaseController.java
@@ -2,7 +2,9 @@ package com.stackfing.admin.controller;
 
 import com.stackfing.admin.service.base.BaseService;
 import com.stackfing.admin.utils.Result;
+import net.bytebuddy.implementation.bind.annotation.Default;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -13,13 +15,17 @@ import org.springframework.web.bind.annotation.*;
 
 public abstract class BaseController<T, R> {
 
+	private static final String DEFAULT_SIZE = "10";
+	private static final String DEFAULT_PAGE = "1";
+
+
 	@Autowired
 	private BaseService<T, R> baseService;
 
 	@GetMapping("")
-	public Result get() {
-		return Result.ok(baseService.list());
-//		throw new RuntimeException("msgssss");
+	public Result get(@RequestParam(value = "page", defaultValue = DEFAULT_PAGE) int page
+			, @RequestParam(value = "size", defaultValue = DEFAULT_SIZE) int size, @RequestParam(value = "desc", defaultValue = "false") Boolean desc) {
+		return Result.ok(baseService.list(size, page));
 	}
 
 	@GetMapping("/{id}")
@@ -35,5 +41,11 @@ public abstract class BaseController<T, R> {
 	@PutMapping("/{id}")
 	public Result put(@RequestBody T model, @PathVariable R id) {
 		return Result.ok(baseService.save(model));
+	}
+
+	@GetMapping("/deleteById/{id}")
+	public Result deleteById(@PathVariable R id) {
+		baseService.delete(id);
+		return Result.ok();
 	}
 }
